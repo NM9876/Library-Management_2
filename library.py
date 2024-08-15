2e1a4dc47b3a995cac5e62011841c9d8c2e13696
NM9876 <newmobile0110@gmail.com>
2024-08-15 18:47:17 +0530

    new changes

--- library.py
+++ library.py
@@ -1,19 +1,44 @@
 from book import Book
+from user import User
 
 class Library:
-   def __init__(self):
-      self.books={}
+    def __init__(self):
+        self.books={}
+        self.users={}
 
-   def add_book(self, book):
-      if book.book_id not in self.books:
-         self.books[book.book_id] = book
-      else:
-         raise Exception("Book already present")
+    def add_book(self, book):
+        if book.book_id not in self.books:
+            self.books[book.book_id]= book
+        else:
+            raise Exception("This book already exists")
+   
+    def remove_book(self, book_id):
+        if book_id in self.books:
+            del books[book_id]
+        else:
+            raise Exception("Book doesn't exist")
+     
+    def register_user(self, user):
+        if user.user_id not in self.users:
+            self.users[user.user_id]= user
+        else:
+            raise Exception("User already exists")
+    def unregister_user(self, user_id):
+        if user_id not in self.users:
+            del self.users[user_id]
+        else:
+            raise Exception("User unregistered")
+    def borrow_book(self, user_id, book_id):
+        if user_id in self.users and book_id in self.books:
+           user = self.users[user_id]
+           book = self.books[book_id]
+           user.borrow_book(book)
+        else:
+           raise Exception("User or book doesn't exists")
       
-   def remove_book(self, book_id):
-      if book_id in books:
-         del books[book_id]
-      else:
-         raise Exception("Book doesn't exist")
-      
-      
+    def list_books(self):
+        return '\n'.join([str(book) for book in self.books.values()])
+    
+    def list_users(self):
+        return '\n'.join([str(user) for user in self.users.values() ])        
+        
--- user.py
+++ user.py
@@ -1,19 +1,23 @@
-class User: 
-    def __init__(self, user_id, name):
-        self.user_id = user_id
-        self.name = name
-        self.borrowed_books = []
-    
-    def borrow_book(self, book):
-        if book.available:
-           book.check_out()
-           self.borrowed_books.append(book)
-        else:
-           raise Exception("Book is not available")
-        
-    def return_book(self, book):
-        if book in self.borrowed_books:
-            book.return_book()
-            self.borrowed_books.remove(book)
-        else:
-            raise Exception("This book was not borrowed by the user")
+class User:
+
+   def __init__(self, user_id, name):
+      self.user_id = user_id
+      self.name = name 
+      self.borrowed_books=[]
+
+   def __str__(self):
+      return f"User ID: {self.user_id}, name: {self.name}"
+   
+   def borrow_book(self, book):
+      if book.available:
+         book.checkout()
+         self.borrowed_books.append(book)
+      else:
+         raise Exception("Book not available.")
+   def return_book(self, book):
+      if book in self.borrowed_books:
+         self.borrowed_books.remove(book)
+      else:
+         raise Exception("This book was not borrowed by the user")
+         
+      
